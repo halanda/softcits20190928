9bde26b528b00c30f1e24b29e4451d3342ffe474
halanda <55902431+halanda@users.noreply.github.com>
2019-10-02 22:02:05 +0800

    技术笔记
    第一节课

---
+++ src/int11.java
@@ -0,0 +1,51 @@
+
+public class int11 {
+
+	public static void main(String[] args) {
+		
+		calculate() ;
+	    int a;
+		int b;
+		int c;
+		
+	}
+	
+	public static void calculate() {
+	   
+		multiple1();
+		int b;
+		int c;
+		int d=b*c;
+		int e;
+		
+		System.out.println(d);
+		
+	}
+	public static int multiple1() {
+		int d;
+		return d;
+	}
+	
+		private static void multiple() {
+			add1();
+			
+		}
+	
+		public static void add1() {
+			
+			int a;
+			int d;
+			int e=a+d;
+			System.out.println(e);	
+		}
+		
+		public static int add(int a,int b,int c) {
+			int e;
+			return e;
+		}
+				
+	}
+
+
+
+}
