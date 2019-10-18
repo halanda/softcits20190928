72b36bafe326bda9f5ac15579d4c6f7ecbdaf692
halanda <55902431+halanda@users.noreply.github.com>
2019-10-18 23:02:44 +0800

    a^2+(b%c-d)*e修正版
    两次multiple区分开

--- src/lesson2/Practice2.java
+++ src/lesson2/Practice2.java
@@ -9,11 +9,16 @@ public class Practice2 {
 		 * 
 		 * 
 		 */
-		Scanner scan=new Scanner (System.in);
+		Scanner scan = new Scanner (System.in);
+		//3
 		int a=scan.nextInt();
+		//5
 		int b=scan.nextInt();
+		//3
 		int c=scan.nextInt();
+		//1
 		int d=scan.nextInt();
+		//5
 		int e=scan.nextInt();
 		
 		int answer=calculate(a,b,c,d,e);
@@ -21,17 +26,16 @@ public class Practice2 {
 	}
 
 	public static int calculate(int a, int b, int c,int d,int e) {
-		int f = multiple(a,a);
+		int f = multiple(a);
 		int g=  mod(b,c);
 		int h = sub(g,d);
-		int i = multiple(h,e);
+		int i = multiple2(h,e);
 		int result = add(f,i);
 		return result;
 	}
-	public static int multiple(int a, int b) {
-		return a*b;
+	public static int multiple(int a) {
+		return a*a;
 	}
-	
 	public static int mod(int a, int b) {
 		return a%b;
 	}
@@ -40,9 +44,10 @@ public class Practice2 {
 		return a-b;
 	}
 	
-	public static int multiple(int a, int b) {
+	public static int multiple2(int a, int b) {
 		return a*b;
 	}
+	
 	
 	public static int add(int a, int b) {
 		return a+b;
