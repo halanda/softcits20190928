7647c5293bceecea3895891efc7ff962b4654d9f
halanda <55902431+halanda@users.noreply.github.com>
2019-10-19 15:22:20 +0800

    Java Loops I
    2 x 1 = 2
    2 x 2 = 4
    2 x 3 = 6
    2 x 4 = 8
    2 x 5 = 10
    2 x 6 = 12
    2 x 7 = 14
    2 x 8 = 16
    2 x 9 = 18
    2 x 10 = 20

--- src/Solution.java
+++ src/Solution.java
@@ -1,48 +1,28 @@
-import java.util.*;
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
 
 
 
-class Solution{
-    public static void main(String []argh)
-    {
+    private static final Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+         for( int i = 1;  i<=10 ; i++) {
 
+        	    int result=N*i;
+        	    
+			System.out.println(N+" "+"x"+" "+ i + " "+"="+" "+result);
+			                   
+		}
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        Scanner sc = new Scanner(System.in);
-        
-        int t=sc.nextInt();
-
-        for(int i=0;i<t;i++)
-        {
-
-            try
-            {
-                long x=sc.nextLong();
-                System.out.println(x+" can be fitted in:");
-                if(x>=-128 && x<=127)System.out.println("* byte");
-                 {
-                    
-           if(x>=java.lang.Short.MIN_VALUE  && x<=java.lang.Short.MAX_VALUE)System.out.println("* short");
-
-    }   
-                {
-                   
-                    if(x>=java.lang.Integer.MIN_VALUE && x<=java.lang.Integer.MAX_VALUE )System.out.println("* int");
-
-    }   
-                
-                {
-                   
-                    if(x>=java.lang.Long.MIN_VALUE && x<=java.lang.Long.MAX_VALUE)System.out.println("* long");
-
-    }   
-            }
-            catch(Exception e)
-            {
-                System.out.println(sc.next()+" can't be fitted anywhere.");
-            }
-
-        }
+        scanner.close();
     }
 }
