49168acf58501754ed920a07af44f1799b205c0d
halanda <55902431+halanda@users.noreply.github.com>
2019-10-28 21:37:55 +0800

    第二次课上练习a+b*c
    第一步：主函数向calculate传值a,b,c
      * 第二步：calculate调用函数multiple，并得到b*c的值
      * 第三步：calculate调用函数add负责a+b*c
      * 第四步：calculate将结果返回给main函数
      * 第五步：main函数将结果打印

---
+++ src/lesson2/FuntionCallDemo.java
@@ -0,0 +1,40 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class FuntionCallDemo {
+ /*a+b*c
+  * 
+  * 第一步：主函数向calculate传值a,b,c
+  * 第二步：calculate调用函数multiple，并得到b*c的值
+  * 第三步：calculate调用函数add负责a+b*c
+  * 第四步：calculate将结果返回给main函数
+  * 第五步：main函数将结果打印
+  * 
+  * 备注运算符：+ - * / %
+  * 
+  */
+	public static void main(String[] args) {
+		
+	  Scanner scan = new Scanner(System.in);
+	  int a = scan.nextInt();
+	  int b = scan.nextInt();
+	  int c = scan.nextInt();
+	  
+	  int answer = calculate(a,b,c);
+	  System.out.println(answer);
+	  	
+	}
+	public static int calculate(int a, int b, int c) {
+		int m = multiple(b,c);
+		int result = add(a,m);
+		return result;
+	}
+	public static int multiple(int a, int b) {
+		return a*b;
+	}
+	public static int add(int a, int b) {
+		return a+b;
+	}
+}
+
