cf8f06cab4b5968196dd23e38f2317ed15c16399
halanda <55902431+halanda@users.noreply.github.com>
2019-10-29 22:22:21 +0800

    计算a+b
    并打印finishied

---
+++ src/lesson2/SolveMeFirst.java
@@ -0,0 +1,27 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class SolveMeFirst {
+
+	public static void main(String[] args) {
+		// º∆À„a+b
+		int a,b;
+		
+		Scanner scan = new Scanner(System.in);
+		//2
+		a=scan.nextInt();
+		//3
+		b=scan.nextInt();
+		
+		int c = solveMeFirst(a, b);
+		System.out.println(c);
+		System.out.println("finished!");
+	
+	}
+  public static int solveMeFirst(int m, int n) {
+          int c = m+n;
+          return c;
+      
+  }
+}
