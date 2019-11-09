2131abd767227c9a988347a5faacaf1a11c4c67f
halanda <55902431+halanda@users.noreply.github.com>
2019-11-09 13:40:09 +0800

    第四节课作业（判断质数）Time Complexity: Primality
    3
    12
    5
    7
    
    Not prime
    Prime
    Prime
    
    https://www.hackerrank.com/challenges/ctci-big-o/problem

--- src/lesson4/Solution.java
+++ src/lesson4/Solution.java
@@ -8,22 +8,34 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class Solution {
+	
+	/* 3 
+	 * 12 
+	 * Not Prime 
+	 * 5 
+	 * Prime 
+	 * 7 
+	 * Prime
+	 */
 
     // Complete the primality function below.
     static String primality(int n) {
     	
     	
 		//判断可不可以被整除
-		if(n==1)return("Not Prime");
-		else if(n==2)return("Prime");
-		
-		for(int i=2;i<n;i++)
-		  if(n%i==0)
-			  return("Not Prime");
-		return("Prime");
+    	if(n==1) return "Not prime";
+        else if(n==2) return "Prime";
+        else if(n%2==0) return "Not prime";
+        else {
+             for(int i=3;i<=java.lang.Math.sqrt(n);i+=2){
+                if(n%i==0) return "Not prime";
+             }
+             
+        return "Prime";
+        
+        }
+    
     }
-    
-    
     
     
 		
