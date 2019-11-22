3f4744f3c4e89d83339c61dff21efb9cd8e7b5d4
halanda <55902431+halanda@users.noreply.github.com>
2019-11-22 21:16:58 +0800

    Java的继承
    https://www.hackerrank.com/challenges/java-inheritance-1/problem
    
    I am walking
    I am flying
    I am singing

---
+++ src/lesson05/model/Bird.java
@@ -0,0 +1,12 @@
+package lesson05.model;
+
+public class Bird extends Animal {
+	public void fly() {
+        System.out.println("I am flying");
+    }
+    public void sing() {
+    	System.out.println("I am singing");
+    }
+	}
+
+
---
+++ src/lesson05/solution.java
@@ -0,0 +1,13 @@
+package lesson05;
+
+import lesson05.model.Bird;
+
+public class solution{
+	   public static void main(String[] args){
+
+	      Bird bird = new Bird();
+	      bird.walk();
+	      bird.fly();
+	      bird.sing(); 
+	   }
+	}
