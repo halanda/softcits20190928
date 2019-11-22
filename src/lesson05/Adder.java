d9c071cf5c20cbfcbfef88245bd52f96cdb2a22c
halanda <55902431+halanda@users.noreply.github.com>
2019-11-22 22:56:46 +0800

    第五次作业第四题-继承
    https://www.hackerrank.com/challenges/java-inheritance-2/problem
    
    两数相加

---
+++ src/lesson05/Adder.java
@@ -0,0 +1,7 @@
+package lesson05;
+
+import lesson05.model.Arithmetic;
+
+public class Adder extends Arithmetic {
+
+}
---
+++ src/lesson05/model/Arithmetic.java
@@ -0,0 +1,10 @@
+package lesson05.model;
+
+public class Arithmetic {
+	
+	    public int add(int x, int y){
+	        return x+y;
+	    }
+	} 
+	
+
