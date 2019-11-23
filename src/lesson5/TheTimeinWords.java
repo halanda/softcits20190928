7b4807f7857b62961d65abb807f427e48a10f0af
halanda <55902431+halanda@users.noreply.github.com>
2019-11-23 10:21:27 +0800

    第五次作业-时钟
    https://www.hackerrank.com/challenges/the-time-in-words/problem

--- src/lesson5/TheTimeinWords.java
+++ src/lesson5/TheTimeinWords.java
@@ -9,93 +9,74 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class TheTimeinWords {
-  //È±¸ö·µ»Ø
-    static String timeInWords(int h, int m) {	
-    	
-//        Scanner scan = new Scanner(System.in);
-//        int n = scan.nextInt();
-//
-//        int[] a = new int[n];
-//        for(int i=0;i<a.length;i++)
-//        	a[i]=scan.nextInt();
-//        
-//        scan.close();
-    
-       
-        String[] numbers= {"one", "two", "three", "four", "five", "six", "seven", 
-                "eight", "nine", "ten", "eleven", "twelve"};
-        
-        String[] numbers1 = {
-                "zero","one","two","three","four","five","six","seven","eight","nine","ten",
-                "eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen",
-                "nineteen","twenty","twenty one","twenty two","twenty three","twenty four",
-                "twenty five","twenty six","twenty seven","twenty eight","twenty nine"
-            };
-        
-    for (int i=0;i<numbers.length;i++) {
-        
-        String currentTime;
-		if (m == 0) {
-            currentTime = numbers[h] + " o' clock";
-        }
 
-        // **** at quarter past ****
+	static String timeInWords(int h, int m) {
 
-        else if (m == 15) {
-            currentTime = "quarter past " + numbers[h];
-        }
+		String[] numbers = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+				"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
+				"twenty", "twenty one", "twenty two", "twenty three", "twenty four", "twenty five", "twenty six",
+				"twenty seven", "twenty eight", "twenty nine" };
 
-        // **** at half hour ****
+		int[] a_arry = new int[30];
+		String currentTime;
+		
+			if (m == 0) {
+				currentTime = numbers[h] + " o' clock";
+			}
 
-        else if (m == 30) {
-            currentTime = "half past " + numbers[h];
-        }
+			// **** at quarter past ****
 
-        // **** at quarter before ****
+			else if (m == 15) {
+				currentTime = "quarter past " + numbers[h];
+			}
 
-        else if (m == 45) {
-            currentTime = "quarter to " + numbers[h + 1];
-        }
+			// **** at half hour ****
 
-        // **** before half hour ****
+			else if (m == 30) {
+				currentTime = "half past " + numbers[h];
+			}
 
-        else if (m < 30) {
-            currentTime = numbers[m] + " minute" + (m <= 1 ? "" : "s") + " past " + numbers[h];
-        }
+			// **** at quarter before ****
 
-        // **** past half hour ****
+			else if (m == 45) {
+				currentTime = "quarter to " + numbers[h + 1];
+			}
 
-        else {
-            currentTime = numbers[60 - m] + " minute" + ((60 - m) <= 1 ? "" : "s") + " to " + numbers[h + 1];
-        }
+			// **** before half hour ****
 
-        // **** display the current time ****
+			else if (m < 30) {
+				currentTime = numbers[m] + " minute" + (m <= 1 ? "" : "s") + " past " + numbers[h];
+			}
 
-        System.out.println(currentTime);
-    }
-        
-        
-        
-    }
-    	
+			// **** past half hour ****
 
-    }
+			else {
+				currentTime = numbers[60 - m] + " minute" + ((60 - m) <= 1 ? "" : "s") + " to " + numbers[h + 1];
+			}
 
-    private static final Scanner scanner = new Scanner(System.in);
+			// **** display the current time ****
 
-    public static void main(String[] args) throws IOException {
+			System.out.println(currentTime);
+			
+			
+		
+		return currentTime;
+	}
 
+	public static void main(String[] args) throws IOException {
 
-        int h = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		final Scanner scanner = new Scanner(System.in);
 
-        int m = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		int h = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        String result = timeInWords(h, m);
+		int m = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        System.out.println(result);
+		String result = timeInWords(h, m);
 
-        scanner.close();
-    }
+		System.out.println(result);
 
+		scanner.close();
+	}
+}
