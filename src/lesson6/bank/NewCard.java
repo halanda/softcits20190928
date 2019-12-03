974a1df9bc03fcac004c64a09dac98d469d58809
halanda <55902431+halanda@users.noreply.github.com>
2019-12-03 22:39:51 +0800

    银行-NewCard
    继承中的
    @Override

---
+++ src/lesson6/bank/NewCard.java
@@ -0,0 +1,13 @@
+package lesson6.bank;
+
+import java.util.Random;
+
+public class NewCard extends AbstractOperation{
+
+	@Override
+	public void operation(int number) {
+		System.out.println("拿到号码" + number);
+		System.out.println("办理新的银行卡...");
+	}
+	
+}
