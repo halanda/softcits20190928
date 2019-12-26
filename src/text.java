94e5158a651b7d92c1a0449b59dea66d1d14e05f
halanda <55902431+halanda@users.noreply.github.com>
2019-12-26 16:20:33 +0800

    复习第一节课，函数调用。add（）；
    键盘输入

--- src/text.java
+++ src/text.java
@@ -1,13 +1,27 @@
+import java.util.Scanner;
 
 public class text {
 
 	public static void main(String[] args) {
-
-		int a=233;
-		byte b=(byte)a;
-		System.out.println(b);
+   
+		add();
 		
-
 	}
 
+	public static void add() {
+		int a = 6;
+		int b = 8;
+		
+//	º¸≈Ã»Î¡¶
+//		Scanner scan = new Scanner(System.in);
+//		int d = scan.nextInt();
+//		int e = scan.nextInt();
+		
+		int c = a + b;
+
+//		byte b = (byte) a;
+		System.out.println(c);
+
+	}
 }
+
