80f8d044e019e280e87dd6dd11469f795d2518ed
halanda <55902431+halanda@users.noreply.github.com>
2020-01-16 21:44:37 +0800

    第9课作业第4题-链表中删除元素Delete a Node
    https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/problem

---
+++ src/lesson9/DeleteaNode.java
@@ -0,0 +1,145 @@
+package lesson9;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class DeleteaNode {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the deleteNode function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode deleteNode(SinglyLinkedListNode head, int position) {
+    	
+//    	if(head == null) {
+//            return head;
+//        }
+//
+//        if (position == 0) {
+//            head = head.next;
+//            return head;
+//        }
+//
+//        SinglyLinkedListNode curr = head;
+//        SinglyLinkedListNode prev = null;
+//        int pos = 0;
+//
+//        while (curr != null) {
+//            if (pos == position) {
+//                prev.next = curr.next;
+//                break;
+//            }
+//            pos++;
+//            prev = curr;
+//            curr = curr.next;
+//        }
+//
+//        return head;
+//    }
+    	
+    	if(head != null && position == 0)
+            return head = head.next;
+        SinglyLinkedListNode curr = head;
+        SinglyLinkedListNode prev = null;
+        int count = 0;
+        while(count < position && curr.next != null){
+            prev = curr;
+            curr = curr.next;
+            count++;
+        }
+        if(curr.next == null)
+            prev.next = null;
+        else{
+            prev.next = curr.next;
+        }
+        return head;
+    	
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < llistCount; i++) {
+            int llistItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            llist.insertNode(llistItem);
+        }
+
+        int position = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        SinglyLinkedListNode llist1 = deleteNode(llist.head, position);
+
+        printSinglyLinkedList(llist1, " ", bufferedWriter);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
