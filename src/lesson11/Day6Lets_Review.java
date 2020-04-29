d9778367414202bcbc0477205fe71fc6e73c1668
halanda <55902431+halanda@users.noreply.github.com>
2020-04-29 12:20:09 +0800

    Day 6: Let's Review    index奇数偶数分开
    https://www.hackerrank.com/challenges/30-review-loop/problem
    
    2
    Hacker
    Rank
    
    Hce akr
    Rn ak

---
+++ src/lesson11/Day6Lets_Review.java
@@ -0,0 +1,39 @@
+package lesson11;
+	import java.io.*;
+	import java.util.*;
+	import java.text.*;
+	import java.math.*;
+	import java.util.regex.*;
+
+	public class Day6Lets_Review {
+
+	    public static void main(String[] args) {
+	        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+	    	Scanner scan = new Scanner(System.in);
+	        int T = scan.nextInt();
+	        String string[] = new String[T];
+	        for(int i = 0; i<T; i++){
+	            string[i] = scan.next();
+	        }
+	        for(int temp = 0; temp<T; temp++){
+
+	            for(int j = 0; j<string[temp].length(); j = j+2)
+	            {
+	                System.out.print(string[temp].charAt(j));
+	            }
+	            System.out.print(" ");
+
+	            for(int j = 1; j<string[temp].length(); j = j+2){
+	                System.out.print(string[temp].charAt(j));
+	            }
+
+	            System.out.println();
+
+	        }
+	        scan.close();
+	    
+	    
+	    
+	    
+	    }
+	}
