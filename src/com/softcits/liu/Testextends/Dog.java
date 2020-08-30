67496855a04515669370e6a8076c9256943ec2a5
halanda <55902431+halanda@users.noreply.github.com>
2020-08-30 15:58:56 +0800

    动物-狗 继承

---
+++ src/com/softcits/liu/Testextends/Dog.java
@@ -0,0 +1,8 @@
+package com.softcits.liu.Testextends;
+
+public class Dog extends Animal{
+	public void run() {
+		System.out.println("π∑≈‹");
+	}
+
+}
---
+++ src/com/softcits/liu/Testextends/Test.java
@@ -0,0 +1,15 @@
+package com.softcits.liu.Testextends;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Dog d = new Dog();
+		System.out.println(d.height);
+		System.out.println(d.name);
+		System.out.println(d.weight);
+		System.out.println(d.getAge());
+	
+	}
+
+}
