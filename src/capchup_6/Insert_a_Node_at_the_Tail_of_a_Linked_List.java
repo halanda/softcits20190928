95433ec46879a69dcfa24d9e887d484faf683626
halanda <55902431+halanda@users.noreply.github.com>
2020-09-11 22:01:07 +0800

    链表第二题，在头插入一个元素
    https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem

---
+++ src/capchup_6/Insert_a_Node_at_the_Tail_of_a_Linked_List.java
@@ -0,0 +1,98 @@
+package capchup_6;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Insert_a_Node_at_the_Tail_of_a_Linked_List {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList() {
+            this.head = null;
+        }
+
+      
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtTail function below.
+
+    /*
+	 * For your reference:
+	 *
+	 * SinglyLinkedListNode { int data; SinglyLinkedListNode next; }
+	 *
+	 */
+	static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+
+		SinglyLinkedListNode toAdd = new SinglyLinkedListNode(data);
+		if (head == null)// to check the first node itself is not null
+		{
+			return toAdd;// if it is null then add the element into this only
+		}
+
+		SinglyLinkedListNode temp = head;
+		while (temp.next != null)// iterate into the entire linkedlist
+		{
+			temp = temp.next;// it will go one after another LL
+		}
+		temp.next = toAdd;// add the element into the last element
+		return head;
+
+	}
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < llistCount; i++) {
+            int llistItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        	
+          SinglyLinkedListNode llist_head = insertNodeAtTail(llist.head, llistItem);
+
+          llist.head = llist_head;
+        }
+
+
+
+        printSinglyLinkedList(llist.head, "\n", bufferedWriter);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
