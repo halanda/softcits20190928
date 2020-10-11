f9a28401b2486555db86bff5326a2aa2c9df6458
halanda <55902431+halanda@users.noreply.github.com>
2020-10-11 10:10:35 +0800

    abi 四舍五入/随机数/绝对值/比较两个数大小

--- src/wu6class/integer.java
+++ src/wu6class/integer.java
@@ -2,6 +2,7 @@ package wu6class;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 public class integer {
 public static void main(String[] args) {
@@ -27,21 +28,36 @@ public static void main(String[] args) {
 	Integer no5 =-128;
 	Integer no6 =-128;
 	System.out.println(no5==no6);
-	
+//	必须有
 	Date d = new Date();
+	Date d1 = new Date();
 //	西方日期表示
-	System.out.println(d);
-//	date转String
+//	System.out.println(d);
+//	定义标准，date转String
 //	MM必须得大写
-	String patten = "yyyy-MM-dd";
+//	String patten = "yyyy-MM-dd";
 	String patten2 = "yyyy-MM-dd hh:mm:ss";
 	String patten1 = "hh-mm-ss";
-	SimpleDateFormat sdf = new SimpleDateFormat(patten);
+	SimpleDateFormat sdf = new SimpleDateFormat(patten2);
 	SimpleDateFormat sdf1 = new SimpleDateFormat(patten1);
 	String timeStr = sdf.format(d);
-	String timeStr1 = sdf1.format(d);
-	System.out.println(timeStr);
-	System.out.println(timeStr1);
+	String timeStr1 = sdf1.format(d1);
+	System.out.println("d的标准"+timeStr);
+	System.out.println("d1的标准"+timeStr1);
+	
+//	方法
+//	比较俩个数大小 得到1
+	Math.min(1, 2);
+//	取绝对值
+	Math.abs(-1);
+//	四舍五入
+	Math.round(123.456);
+//	随机数
+	Random random = new Random();
+//	【0~9）
+	System.out.println(random.nextInt(9));
+	
+	
 	
 	
 }
