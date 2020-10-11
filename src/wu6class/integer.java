55f9a0733f13fb9952cf21975126d40f3fdecf70
halanda <55902431+halanda@users.noreply.github.com>
2020-10-11 09:50:01 +0800

    SimpleDateFormat 日期格式转换

--- src/wu6class/integer.java
+++ src/wu6class/integer.java
@@ -1,5 +1,8 @@
 package wu6class;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class integer {
 public static void main(String[] args) {
 	
@@ -24,6 +27,23 @@ public static void main(String[] args) {
 	Integer no5 =-128;
 	Integer no6 =-128;
 	System.out.println(no5==no6);
+	
+	Date d = new Date();
+//	西方日期表示
+	System.out.println(d);
+//	date转String
+//	MM必须得大写
+	String patten = "yyyy-MM-dd";
+	String patten2 = "yyyy-MM-dd hh:mm:ss";
+	String patten1 = "hh-mm-ss";
+	SimpleDateFormat sdf = new SimpleDateFormat(patten);
+	SimpleDateFormat sdf1 = new SimpleDateFormat(patten1);
+	String timeStr = sdf.format(d);
+	String timeStr1 = sdf1.format(d);
+	System.out.println(timeStr);
+	System.out.println(timeStr1);
+	
+	
 }
 	
 	
