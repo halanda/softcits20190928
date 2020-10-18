374aebc5f3ad1ec75ad616b25057df0126d5c49a
halanda <55902431+halanda@users.noreply.github.com>
2020-10-18 13:47:51 +0800

    output

---
+++ src/wu7class/Fileoutput.java
@@ -0,0 +1,25 @@
+package wu7class;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Fileoutput {
+public static void main(String[] args) {
+	File file = new File("D:\\test3.txt");
+	
+	try {
+		file.createNewFile();
+FileOutputStream fos = new FileOutputStream(file);
+		
+		String str = "ÎÒ°®ÄãÖÐ¹ú";
+		byte [] arr = str.getBytes();
+		fos.write(arr);
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+}
+}
