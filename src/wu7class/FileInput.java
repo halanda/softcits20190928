65467455c830e116435cfaeb3daedb0b48557d9f
halanda <55902431+halanda@users.noreply.github.com>
2020-10-18 13:43:57 +0800

    FileInput
    没那么多try catch的写法

--- src/wu7class/FileInput.java
+++ src/wu7class/FileInput.java
@@ -7,26 +7,19 @@ import java.io.IOException;
 
 public class FileInput {
 public static void main(String[] args) {
-	File file = new File("D:\\test2.txt");
+	File file = new File("D:\\test3.txt");
 	
 	try {
 		file.createNewFile();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	try {
-		FileOutputStream fos = new FileOutputStream(file);
+FileOutputStream fos = new FileOutputStream(file);
 		
 		String str = "ÎÒ°®ÄãÖÐ¹ú";
 		byte [] arr = str.getBytes();
 		fos.write(arr);
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
 	} catch (IOException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
+	
 }
 }
