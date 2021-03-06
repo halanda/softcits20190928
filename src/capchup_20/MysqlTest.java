d9a337637538a03617ab25ef29982592d1f816a5
halanda <55902431+halanda@users.noreply.github.com>
2021-03-06 15:54:11 +0800

    把数据库内容做成list

--- src/capchup_20/MysqlTest.java
+++ src/capchup_20/MysqlTest.java
@@ -1,8 +1,13 @@
 package capchup_20;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MysqlTest {
 
 	public static void main(String[] args) throws ClassNotFoundException {
+		List <ProductBean> list = new ArrayList<>();
+		
 		// load mysql driver
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		try {
@@ -13,9 +18,16 @@ public class MysqlTest {
 				ResultSet RS = stmt.executeQuery("select * from product_tbl");
 					while (RS.next()) {
 						System.out.println(RS.getInt(1) + " " + RS.getString(2) + " " + RS.getDouble(3) + " " + RS.getDate(4));
+						ProductBean productBean = new ProductBean();
+						productBean.setId (RS.getInt(1));
+						productBean.setProduct_name(RS.getString(2));
+						productBean.setPrice(RS.getDouble(3));
+						productBean.setOnboard_date(RS.getDate(4));
+						list.add(productBean);
+					
 					}
 		
-		
+		          System.out.println("Total objects:"+ list.size());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}	
